842e7fd2b3a4b2892d414a3b572a3aeff763cc38
Julia Muiruri <julia.muiruri4@gmail.com>
2025-02-18 04:59:10 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillMember() {
+    // Create a new object
+    var member = new Object();
+    // Add properties to the object
+    member.name = "John";
+    member.age = 30;
+    // Return the object
+    return member;
+}
